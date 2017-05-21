ff4ff7a791cf42adecc3adb640794d6612b7c914
crackhopper <crackhopper@gmail.com>
2017-05-21 13:08:43 +0800

    split time extractor

--- extracted.py
+++ extracted.py
@@ -3,3 +3,6 @@ from config import *
 import pandas as pd
 import os
 extracted = pd.HDFStore(os.path.join(extractDir,'%s.db'%extractor_name))
+
+def load_extract_by_name(name):
+  return pd.HDFStore(os.path.join(extractDir,'%s.db'%name))
